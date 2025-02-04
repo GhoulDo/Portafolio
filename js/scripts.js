1bf37cb968fcef038080ff57fdad4ccf43c41389
GhoulDo <joooohanenciso@gmail.com>
2025-02-04 16:42:07 -0500

    Foto de perfil y estilos

--- js/scripts.js
+++ js/scripts.js
@@ -35,4 +35,23 @@ document.addEventListener('DOMContentLoaded', function() {
             alert('Hubo un problema al enviar el formulario.');
         });
     });
+
+    // Animations on scroll
+    const sections = document.querySelectorAll('section');
+    const options = {
+        threshold: 0.1
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, options);
+
+    sections.forEach(section => {
+        observer.observe(section);
+    });
 });
